80f274eed68c11183f665df089e31ea29f3eaa2f
Semyon <semen7721@gmail.com>
2022-06-10 20:40:33 +0300

    Fixed bugs with lists

--- Modular_project/src/Modular_project.c
+++ Modular_project/src/Modular_project.c
@@ -15,7 +15,6 @@
 #include "Queue.h"
 #include "Stack.h"
 #include "Input.h"
-#include "Output.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -86,7 +85,7 @@ int main(int argc, char* argv[]){
 				fclose(fout);
 				break;
 			case 'p':
-				printf("С какой структурой вы хотите работать? l - список, q - очередь");
+				printf("С какой структурой вы хотите работать? l - список, q - очередь\n");
 				scanf(" %c", &structure);
 				switch (structure) {
 					case 'l':
@@ -103,13 +102,98 @@ int main(int argc, char* argv[]){
 							pushElement(&l1, var);
 						}
 						fclose(fin);
-						calcList(l1 ,l2);
+						l1.current = l1.head;
+						while (l1.current != NULL){
+							var->mode = l1.current->mode;
+							var->operation = l1.current->operation;
+							var->size = l1.current->size;
+							var->x = l1.current->x;
+							var->y = l1.current->y;
+							switch (var->mode){
+								case 'v':
+									switch (var->operation){
+										case '+':
+											var->result = doSumVector(var->x, var->y, var->size);
+											break;
+										case '-':
+											var->result = doSubstractionVector(var->x, var->y, var->size);
+											break;
+										case '*':
+											var->result = doMultiplyVector(var->x, var->y, var->size);
+											break;
+									}
+									break;
+								case 'n':
+									switch (l1.current->operation){
+										case '+':
+											var->result = doSum(*var->x, *var->y);
+											break;
+										case '-':
+											var->result = doSubstraction(*var->x, *var->y);
+											break;
+										case '*':
+											var->result = doMultiply(*var->x, *var->y);
+											break;
+										case '/':
+											var->result = doDivision(*var->x, *var->y);
+											break;
+										case '!':
+											var->result = doFactorial(*var->x);
+											break;
+										case '^':
+											var->result = doPow(*var->x, *var->y);
+											break;
+									}
+									break;
+							}
+							pushElement(&l2, var);
+							nextElement(&l1);
+						}
+						free(var);
 						l1.current = l1.head;
 						l2.current = l2.head;
 						if ((fout = fopen(output, "a")) == NULL){
 							fout = fopen(output, "w");
 						}
-						writeList(fout, l2);
+						while (l2.current != NULL){
+							switch (l2.current->mode){
+								case 'v':
+									fprintf(fout, "( ");
+									for (int i = 0; i < l2.current->size; i++){
+										if (i == l2.current->size - 1){
+											fprintf(fout, "%lf", l2.current->x[i]);
+										}
+										else fprintf(fout, "%lf ", l2.current->x[i]);
+									}
+									fprintf(fout, " ) %c ( ", l2.current->operation);
+									for (int i = 0; i < l2.current->size; i++){
+										if (i == l2.current->size - 1){
+											fprintf(fout, "%lf", l2.current->y[i]);
+										}
+										else fprintf(fout, "%lf ", l2.current->y[i]);
+									}
+									if (l2.current->operation == '+' || l2.current->operation == '-'){
+										fprintf(fout, " ) = ( ");
+										for (int i = 0; i < l2.current->size; i++){
+											if (i == l2.current->size - 1){
+												fprintf(fout, "%lf", l2.current->result[i]);
+											}
+											else fprintf(fout, "%lf ", l2.current->result[i]);
+										}
+										fprintf(fout, " )\n");
+									}
+									else{
+										fprintf(fout, " ) = %lf\n", *l2.current->result);
+									}
+									break;
+								case 'n':
+									if (l2.current->operation != '!') {
+										fprintf(fout, "%lf + %lf = %lf\n", *l2.current->x, *l2.current->y, *l2.current->result);
+									}
+									else fprintf(fout, "%lf! = %lf\n", *l2.current->x, *l2.current->result);
+							}
+							nextElement(&l2);
+						}
 						fclose(fout);
 						while (l1.head != NULL){
 							deleteElement(&l1);
@@ -131,11 +215,89 @@ int main(int argc, char* argv[]){
 							var = inputPN(fin);
 							enqueue(&q1, var);
 						}
-						calcQueue(q1, q2);
-						if ((fout = fopen(output, "a")) == NULL){
+						while ((var = dequeue(&q1)) != NULL) {
+							switch (var->mode) {
+								case 'v':
+									switch (var->operation) {
+										case '+':
+											var->result = doSumVector(var->x, var->y, var->size);
+											break;
+										case '-':
+											var->result = doSubstractionVector(var->x, var->y, var->size);
+											break;
+										case '*':
+											var->result = doMultiplyVector(var->x, var->y, var->size);
+											break;
+									}
+									break;
+								case 'n':
+									switch (var->operation) {
+										case '+':
+											var->result = doSum(*var->x, *var->y);
+											break;
+										case '-':
+											var->result = doSubstraction(*var->x, *var->y);
+											break;
+										case '*':
+											var->result = doMultiply(*var->x, *var->y);
+											break;
+										case '/':
+											var->result = doDivision(*var->x, *var->y);
+											break;
+										case '!':
+											var->result = doFactorial(*var->x);
+											break;
+										case '^':
+											var->result = doPow(*var->x, *var->y);
+											break;
+									}
+									break;
+							}
+							enqueue(&q2, var);
+						}
+						if ((fout = fopen(output, "a")) == NULL) {
 							fout = fopen(output, "w");
 						}
-						writeQueue(fout, q2);
+						while ((var = dequeue(&q2)) != NULL) {
+							switch (var->mode) {
+								case 'v':
+									fprintf(fout, "( ");
+									for (int i = 0; i < var->size; i++) {
+										if (i == var->size - 1){
+											fprintf(fout, "%lf", var->x[i]);
+										}
+										else fprintf(fout, "%lf ", var->x[i]);
+									}
+									fprintf(fout, " ) %c ( ", var->operation);
+									for (int i = 0; i < var->size; i++){
+										if (i == var->size - 1){
+											fprintf(fout, "%lf", var->y[i]);
+										}
+										else fprintf(fout, "%lf ", var->y[i]);
+									}
+									if (var->operation == '+' || var->operation == '-') {
+										fprintf(fout, " ) = ( ");
+										for (int i = 0; i < var->size; i++) {
+											if (i == var->size - 1){
+												fprintf(fout, "%lf", var->result[i]);
+											}
+											else fprintf(fout, "%lf ", var->result[i]);
+										}
+										fprintf(fout, " )\n");
+									}
+									else{
+										fprintf(fout, " ) = %lf\n", *var->result);
+									}
+									break;
+								case 'n':
+									if (var->operation != '!') {
+										fprintf(fout, "%lf + %lf = %lf\n", *var->x, *var->y, *var->result);
+									}
+									else fprintf(fout, "%lf! = %lf\n", *var->x, *var->result);
+									}
+									break;
+							}
+						fclose(fout);
 						break;
 				}
 				break;
